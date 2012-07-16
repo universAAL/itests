5bb89a8322b1ab0e73895f76a2ff5a189b83c071
Marek Psiuk <marek.psiuk@gmail.com>
2012-07-16 10:30:07 +0000

    - preparing for release: itests-conf, itests, uaal-maven-plugin

--- itests/src/main/java/org/universAAL/itests/IntegrationTest.java
+++ itests/src/main/java/org/universAAL/itests/IntegrationTest.java
@@ -689,7 +689,7 @@ public class IntegrationTest extends AbstractConfigurableBundleCreatorTests {
 			new UrlResource(
 				"mvn:org.apache.commons/com.springsource.org.apache.commons.io/1.4.0"));
 	bundles.add(0, new UrlResource(
-		"mvn:org.universAAL.support/itests/1.1.3-SNAPSHOT"));
+		"mvn:org.universAAL.support/itests/1.2.0"));
 	bundles.add(0, new UrlResource(
 		"mvn:org.ops4j.pax.url/pax-url-wrap/1.3.5"));
 	bundles.add(0, new UrlResource(
