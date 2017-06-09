6d27f4d58d72b708621fe785379d311f2f05f4a9
cstockloew <stockloew@gmx.de>
2017-06-09 05:47:12 +0200

    source formatting

--- itests-conf/src/main/java/org/universAAL/itests/conf/IntegrationTestConsts.java
+++ itests-conf/src/main/java/org/universAAL/itests/conf/IntegrationTestConsts.java
@@ -31,31 +31,29 @@ import java.util.Properties;
  * 
  */
 public class IntegrationTestConsts {
-    public static String RUN_DIR_GROUP_ID = "org.universAAL.support";
+	public static String RUN_DIR_GROUP_ID = "org.universAAL.support";
 
-    public static String RUN_DIR_ARTIFACT_ID = "itests-rundir";
+	public static String RUN_DIR_ARTIFACT_ID = "itests-rundir";
 
-    public static String SEPARATED_ARTIFACT_DEPS = "target/separated_artifact_deps";
+	public static String SEPARATED_ARTIFACT_DEPS = "target/separated_artifact_deps";
 
-    public static String TEST_COMPOSITE = "target/artifact-test.composite";
+	public static String TEST_COMPOSITE = "target/artifact-test.composite";
 
-    public static String getRunDirMvnUrl() {
-	String rundirVersion = MavenUtilsRunDir.getArtifactVersion(
-		IntegrationTestConsts.RUN_DIR_GROUP_ID,
-		IntegrationTestConsts.RUN_DIR_ARTIFACT_ID);
-	return String.format("mvn:%s/%s/%s",
-		IntegrationTestConsts.RUN_DIR_GROUP_ID,
-		IntegrationTestConsts.RUN_DIR_ARTIFACT_ID, rundirVersion);
-    }
-    
-    public static Properties getuAALMWProperties(){
-	try {
-	    URL prop = IntegrationTestConsts.class.getResource("/uAALmw.properties");
-	    Properties p = new Properties();
-	    p.load(prop.openStream());
-	    return p;
-	} catch (Exception e) {
-	    return new Properties();
+	public static String getRunDirMvnUrl() {
+		String rundirVersion = MavenUtilsRunDir.getArtifactVersion(IntegrationTestConsts.RUN_DIR_GROUP_ID,
+				IntegrationTestConsts.RUN_DIR_ARTIFACT_ID);
+		return String.format("mvn:%s/%s/%s", IntegrationTestConsts.RUN_DIR_GROUP_ID,
+				IntegrationTestConsts.RUN_DIR_ARTIFACT_ID, rundirVersion);
 	}
-    }
+
+	public static Properties getuAALMWProperties() {
+		try {
+			URL prop = IntegrationTestConsts.class.getResource("/uAALmw.properties");
+			Properties p = new Properties();
+			p.load(prop.openStream());
+			return p;
+		} catch (Exception e) {
+			return new Properties();
+		}
+	}
 }
